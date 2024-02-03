d3b0b15897413cf974e4a92744e7f5ccc2433226
abhish2 <119543163+abhish2@users.noreply.github.com>
2024-02-03 16:20:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,24 @@
 import streamlit as st
-import folium
-import requests
-import pandas as pd
-from geopy.distance import geodesic
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-# Function to check if the current location is inside any of the geofences
-def is_outside_geofences(current_location, geofences):
-    for geofence_center, radius in geofences:
-        distance = geodesic(current_location, geofence_center).meters
-        if distance <= radius:
-            return False
-    return True
+loc_button = Button(label="Get Location")
+loc_button.js_on_event("button_click", CustomJS(code="""
+    navigator.geolocation.getCurrentPosition(
+        (loc) => {
+            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+        }
+    )
+    """))
+result = streamlit_bokeh_events(
+    loc_button,
+    events="GET_LOCATION",
+    key="get_location",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
 
-# Function to get user location based on IP address using the IPinfo API
-@st.cache
-def get_user_location():
-    try:
-        response = requests.get("http://ipinfo.io")
-        data = response.json()
-        return data.get("loc", "").split(",")
-    except Exception as e:
-        st.warning(f"Error getting user location: {e}")
-        return None
-
-# Function to read geofence data from CSV and create a list of tuples
-def read_geofences_from_csv(csv_path):
-    df = pd.read_csv(csv_path)
-    geofences = [((row['Latitude'], row['Longitude']), row['Radius']) for _, row in df.iterrows()]
-    return geofences
-
-# Streamlit app
-def main():
-    st.title("Road Accident App")
-
-    # Specify the path to your CSV file containing geofence data
-    csv_path = "gv.csv"
-
-    # Read geofence data from CSV and create the geofences list
-    geofences = read_geofences_from_csv(csv_path)
-
-    # Get user location based on IP address
-    user_location = get_user_location()
-
-    if user_location:
-        st.write(f"User Location (based on IP): Latitude: {user_location[0]}, Longitude: {user_location[1]}")
-
-        # Check if the user's location is outside any of the geofences
-        outside_geofences = is_outside_geofences(user_location, geofences)
-
-        # Display result
-        if outside_geofences:
-            st.success("Safe to navigate")
-        else:
-            st.error("Beware! You are in an Accident Prone Region")
-
-        # Display the Folium map with geofences and user's location
-        m = folium.Map(location=(float(user_location[0]), float(user_location[1])), zoom_start=5)
-
-        # Plot each geofence on the map
-        for geofence_center, radius in geofences:
-            folium.Circle(geofence_center, radius=radius, color='red', fill=True, fill_color='red').add_to(m)
-
-        folium.Marker((float(user_location[0]), float(user_location[1])), popup="User Location", icon=folium.Icon(color='blue')).add_to(m)
-
-        # Convert Folium map to HTML and display using st.markdown
-        map_html = m._repr_html_()
-        st.markdown(map_html, unsafe_allow_html=True)
-
-if __name__ == "__main__":
-    main()
+if result:
+    if "GET_LOCATION" in result:
+        st.write(result.get("GET_LOCATION"))
